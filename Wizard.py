975c1e26e62aa4a3c9e3539f9bd23c3fae660430
Ahmed Osama Afefy <32176872+ahmedosama07@users.noreply.github.com>
2021-09-12 02:36:37 +0200

    Add files via upload

---
+++ Wizard.py
@@ -0,0 +1,78 @@
+# import numpy library to use it for arrays
+import numpy as np
+
+
+# wizard class
+class Wizard:
+    # reading from file
+    all_spells = np.loadtxt("spells.txt", usecols=range(0, 3), dtype='str')
+    wizard_spells = []
+
+    # constructor takes wizard's name as argument
+    def __init__(self, name):
+        # setting wizard's attributes
+        self.name = name
+        self.shield_limit = 3
+        self.energy = 500
+        self.health = 100
+        for spell in self.all_spells:
+            if spell[0] == "A" or spell[0] == self.name[0]:
+                self.wizard_spells = np.append(self.wizard_spells, spell[1:])
+
+        self.wizard_spells = self.wizard_spells.reshape(int(len(self.wizard_spells) / 2), 2)
+
+    # performing spells on opponent
+    def attack(self, spell, opponent_shield):
+        # handling ValueError for the index of the spell
+        try:
+            # getting the index of the spell to know its power
+            i = int(str(np.where(self.wizard_spells == spell))[8])
+            power = int(self.wizard_spells[i, 1])
+
+            # subtracting spell's power from wizard's energy
+            self.energy = self.energy - power
+            if self.energy < 0:
+                self.energy = 0
+            if opponent_shield:
+                return 0
+            else:
+                return power
+        except ValueError:
+            print("not spell")
+            return -1
+
+    # check for spell
+    def is_spell(self, spell):
+        try:
+            return int(str(np.where(self.wizard_spells == spell))[8])
+        except ValueError:
+            print("not spell")
+            return -1
+
+    # energy getter
+    def wizard_energy(self):
+        return self.energy
+
+    # health getter
+    def wizard_health(self):
+        return self.health
+
+    # decrementing health
+    def decrease_power(self, power_difference):
+        if power_difference > self.health:
+            self.health = 0
+        else:
+            self.health = self.health - power_difference
+        return self.health
+
+    # using the shield
+    def shield_used(self):
+        if self.shield_limit == 0:
+            return -1
+        else:
+            self.shield_limit -= 1
+            return self.shield_limit
+
+    # print winner's name
+    def winner(self):
+        print(f"        {self.name} is winner ...")
---
+++ main.py
@@ -0,0 +1,70 @@
+# importing wizard class
+from Wizard import Wizard
+
+# creating two objects for the two wizards
+harry = Wizard("Harry Potter")
+voldemort = Wizard("Voldemort")
+
+# game loop
+while harry.health > 0 and voldemort.health > 0:
+    # reading spells as input
+    print("Enter the two spells (harry then voldemort)")
+    harry_spell, voldemort_spell = input().split()
+
+    # checking if spell is valid
+    if harry.is_spell(harry_spell) == -1 or voldemort.is_spell(voldemort_spell) == -1:
+        continue
+
+    # checking if shield is used
+    voldemort_shield = False
+    harry_shield = False
+    if harry_spell == "sheild":
+        harry_shield_status = harry.shield_used()
+        if harry_shield_status == -1:
+            harry_shield = False
+        else:
+            harry_shield = True
+    if voldemort_spell == "sheild":
+        voldemort_shield_status = voldemort.shield_used()
+        if voldemort_shield_status == -1:
+            voldemort_shield = False
+        else:
+            voldemort_shield = True
+
+    # power of the spell
+    harry_attack_power = harry.attack(harry_spell, voldemort_shield)
+    voldemort_attack_power = voldemort.attack(voldemort_spell, harry_shield)
+
+    # wizard's energy
+    harry_energy = harry.wizard_energy()
+    voldemort_energy = voldemort.wizard_energy()
+
+    # power difference between two spells
+    power_difference = harry_attack_power - voldemort_attack_power
+
+    # wizard's health
+    harry_power = harry.wizard_health()
+    voldemort_power = voldemort.wizard_health()
+
+    # decrementing health
+    if power_difference > 0:
+        voldemort_power = voldemort.decrease_power(power_difference)
+    elif power_difference < 0:
+        harry_power = harry.decrease_power(abs(power_difference))
+
+    # printing results
+    print("         Harry                Voldemort")
+    print(f"Health : {harry_power}                      {voldemort_power}")
+    print(f"Energy : {harry_energy}                      {voldemort_energy}")
+
+    # if a wizard uses all his energy he dies
+    if harry_energy == 0 and voldemort_energy > 0:
+        harry_power = 0
+    elif harry_energy > 0 and voldemort_energy == 0:
+        voldemort_power = 0
+
+# game end
+if harry.health == 0 and voldemort.health != 0:
+    voldemort.winner()
+elif harry.health != 0 and voldemort.health == 0:
+    harry.winner()
